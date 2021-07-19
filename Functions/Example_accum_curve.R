6913aacac30b1b71e4a47c0aea6f0070f53c7d4a
kahoCF <84483422+kahoCF@users.noreply.github.com>
2021-07-19 12:14:44 -1000

    Changed order of examples and adjusted example code for Threnetes ruckeri. Added second example for compare.aco.

--- Functions/Example_accum_curve.R
+++ Functions/Example_accum_curve.R
@@ -1,7 +1,15 @@
 # Laura Jim?nez
 # April, 2021
 
-# Working directory and libraries ----------------------------------------------
+# Description: -----------------------------------------------------------------
+# In the following are the examples for the function accum.occ3 and compare.aco
+# for both species: Catasticta nimbice (cn) and Threnetes ruckeri (thr). The models
+# used are the weighted normal distribution (wn) and Mahalanobis distance (maha).
+
+
+# Main: How the functions work (examples) --------------------------------------
+
+# Working directory and libraries -----------------------------
 library(dismo)
 library(maptools)
 library(rgdal)
@@ -12,22 +20,102 @@ library(raster)
 source(".\\Functions\\Accumulation_curve_test.R")
 
 
-## Example version 2 -----------------
-# Parameters
-# Occurrence data 
-cn.occ <- read.csv(".\\Catasticta_nimbice_occ_G.csv",header=T)
-thr.occ <- read.csv(".\\Threnetes_ruckeri_occ_G.csv",header=T)
+# Curve Example 1: Catasticta nimbice -------------------------
 
-# read rasters with suitability index (from Niche.G), a polygon of the study area and crop
+## Load necessary data and prepare parameters
+
+# Occurrence data, table with three columns
+cn.occ <- read.csv(".\\Catasticta_nimbice_occ_G.csv",header=T)
+
+# read cropped rasters with suitability index (from Niche.G) and cropped rasters
+#   with environmental information
 cn.wnc <- raster("./Rasters/Catasticta_nimbice_wn_cropped.tif")
 cn.mahac <- raster("./Rasters/Catasticta_nimbice_maha_cropped.tif")
+
+bio1cn <- raster("./Rasters/Catasticta_nimbice_bio1_cropped.tif")
+bio12cn <- raster("./Rasters/Catasticta_nimbice_bio12_cropped.tif")
+
+# set names for species and layers 
+spname <- "Catasticta_nimbice"
+bio1 <- "Annual mean temperature (°C*10)"
+bio12 <- "Annual precipitation (mm)"
+
+# create suitability stacks for Catasticta nimbice
+cn.wncE <- stack(cn.wnc, bio1cn, bio12cn)
+cn.mahacE <- stack(cn.mahac, bio1cn, bio12cn)
+
+## apply function for both models
+cnwn.test3 <- accum.occ3(names = c(spname,bio1,bio12),G.occ = cn.occ,
+                         suit.Estck = cn.wncE,null.mod="hypergeom",clev=0.95)
+cnmaha.test3 <- accum.occ3(names = c(spname,bio1,bio12),G.occ = cn.occ,
+                           suit.Estck = cn.mahacE,null.mod="hypergeom",clev=0.95)
+
+
+
+# Model Comparison Example 1: Catasticta nimbice ---------------------------
+
+# create a list with all the matrices using only the first two columns 
+models <- list(cnmaha.test3[,1:2], cnwn.test3[,1:2])
+
+# Apply the function that draws all the models in a single plot
+compare.aco(mods=models, nocc=155, ncells=7251,
+            sp.name=spname, mods.names=c("Mahalanobis","Weigthed-Normal"),alpha=0.95)
+
+
+
+
+# Curve Example 1: Threnetes ruckeri -------------------------
+
+## Load necessary data and prepare parameters
+
+# Occurrence data, table with three columns
+thr.occ <- read.csv(".\\Threnetes_ruckeri_occ_G.csv",header=T)
+
+# read cropped rasters with suitability index (from Niche.G) and cropped rasters
+#   with environmental information
+
 thr.wnc <- raster("./Rasters/Threnetes_ruckeri_wn_cropped.tif")
 thr.mahac <- raster("./Rasters/Threnetes_ruckeri_maha_cropped.tif")
 
+bio1thr <- raster("./Rasters/Threnetes_ruckeri_bio1_cropped.tif")
+bio12thr <- raster("./Rasters/Threnetes_ruckeri_bio12_cropped.tif")
 
-# Species name
-spname <- "Catasticta_nimbice"
+# set names for species and layers (layernames have already been set in prev. ex.)
 spname2 <- "Threnetes_ruckeri"
+
+# create suitability stack for Threnetes ruckeri
+thr.wncE <- stack(thr.wnc, bio1thr, bio12thr)
+thr.mahacE <- stack(thr.mahac, bio1thr, bio12thr)
+
+
+## apply function for both models
+thrwn.test3 <- accum.occ3(names = c(spname2, bio1, bio12),G.occ = thr.occ,
+                          suit.Estck = thr.wncE,null.mod="hypergeom",clev=0.95)
+thrmaha.test3 <- accum.occ3(names = c(spname2, bio1, bio12),G.occ = thr.occ,
+                            suit.Estck = thr.mahacE,null.mod="hypergeom",clev=0.95)
+
+
+
+
+# Model Comparison Example 2: Threnetes ruckeri ---------------------------
+
+# create a list with all the matrices using only the first two columns 
+models2 <- list(thrmaha.test3[,1:2], thrwn.test3[,1:2])
+
+# Apply the function that draws all the models in a single plot
+compare.aco(mods=models2, nocc=171, ncells=4170,
+            sp.name=spname2, mods.names=c("Mahalanobis","Weigthed-Normal"),alpha=0.95)
+
+
+
+
+
+
+
+
+###################################### delete some time ###################
+
+## Example version 2 -----------------
 
 # Apply evaluation method
 # cn
@@ -42,49 +130,6 @@ thrmaha.test <- accum.occ2(spname, output.mod=thr.mahac, occ.pnts=thr.occ,
 thrwn.test <- accum.occ2(spname, output.mod=thr.wnc, occ.pnts=thr.occ,
                         null.mod="hypergeom", conlev=0.95)
 
-
-## Example version 3 --------------------
-# Parameters
-# same occurrence data and species name, and cropped raster as above
-
-# create suitability stack for Catasticta nimbice
-bio1cn <- raster("./Rasters/Catasticta_nimbice_bio1_cropped.tif")
-bio12cn <- raster("./Rasters/Catasticta_nimbice_bio12_cropped.tif")
-bio1 <- "Annual mean temperature (°C*10)"
-bio12 <- "Annual precipitation (mm)"
-
-cn.wncE <- stack(cn.wnc, bio1cn, bio12cn)
-cn.mahacE <- stack(cn.mahac, bio1cn, bio12cn)
-
-# apply function
-cnwn.test3 <- accum.occ3(names = c(spname,bio1,bio12),G.occ = cn.occ,
-                           suit.Estck = cn.wncE,null.mod="hypergeom",clev=0.95)
-cnmaha.test3 <- accum.occ3(names = c(spname,bio1,bio12),G.occ = cn.occ,
-                           suit.Estck = cn.mahacE,null.mod="hypergeom",clev=0.95)
-
-# create suitability stack for Threnetes ruckeri
-bio1thr <- raster("./Rasters/Threnetes_ruckeri_bio1_cropped.tif")
-bio12thr <- raster("./Rasters/Threnetes_ruckeri_bio12_cropped.tif")
-
-thr.wncE <- stack(thr.wnc, bio1thr, bio12thr)
-thr.mahacE <- stack(thr.mahac, bio1thr, bio12thr)
-
-# apply function
-thrwn.test3 <- accum.occ3(sp.name = spname2,G.occ = thr.occ,
-                         suit.Estck = thr.wncE,null.mod="hypergeom",clev=0.95)
-thrmaha.test3 <- accum.occ3(sp.name = spname2,G.occ = thr.occ,
-                           suit.Estck = thr.mahacE,null.mod="hypergeom",clev=0.95)
-
-
-
-
-# Comparing the models ---------------------------------------------------------
-# create a list with all the matrices using only the first two columns 
-models <- list(cnmaha.test3[,1:2], cnwn.test3[,1:2])
-
-# Apply the function that draws all the models in a single plot
-compare.aco(mods=models, nocc=155, ncells=7251,
-             sp.name=spname, mods.names=c("Mahalanobis","Weigthed-Normal"),alpha=0.95)
 
 
 #
